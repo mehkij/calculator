9c9356b4ce0d8bea15b4e1150e9caae43753e49b
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-09-18 13:29:32 -0400

    Add operate function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+// let operand1
+// let operand2
+// let operation
+
 function add(operand1, operand2) {
   return operand1 + operand2;
 }
@@ -12,4 +16,16 @@ function multiply(operand1, operand2) {
 
 function divide(operand1, operand2) {
   return operand1 / operand2;
+}
+
+function operate(operator, operand1, operand2) {
+  if (operator == "+") {
+    return add(operand1, operand2);
+  } else if (operator == "-") {
+    return subtract(operand1, operand2);
+  } else if (operator == "*") {
+    return multiply(operand1, operand2);
+  } else if (operator == "/") {
+    return divide(operand1, operand2);
+  }
 }
