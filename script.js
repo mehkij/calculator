2e75cf79394f61ae42545781203f322dc6b75c88
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-09-19 14:07:51 -0400

    Update display function

--- script.js
+++ script.js
@@ -2,6 +2,9 @@
 // let operand2
 // let operation
 
+const display = document.querySelector(".display");
+const displayOutput = document.querySelector("#display-output");
+
 function add(operand1, operand2) {
   return operand1 + operand2;
 }
@@ -29,3 +32,25 @@ function operate(operator, operand1, operand2) {
     return divide(operand1, operand2);
   }
 }
+
+// button event handling
+
+const buttons = document.querySelector(".buttons button");
+
+function updateDisplay() {
+  buttons.addEventListener("click", (e) => {
+    // e.stopPropagation();
+    const clickedButton = e.target;
+    let currentDisplay = 0;
+    
+    if (clickedButton.id === "clear" || clickedButton.id === "multiplication" || clickedButton.id === "division" || clickedButton.id === "subtraction" || clickedButton.id === "addition" || clickedButton.id === "equals") {
+      displayOutput.textContent = currentDisplay;
+      display.appendChild(displayOutput);
+    } else {
+      displayOutput.textContent = clickedButton.textContent;
+      display.appendChild(displayOutput);
+    }
+  });
+}
+
+updateDisplay();
