9a997a8f29969f3d8e88eada51e1ec9c2567c240
Mehki Joseph <109314751+mehkij@users.noreply.github.com>
2023-09-21 15:44:46 -0400

    Refactor

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const display = document.querySelector("#display-output");
 const buttons = document.querySelectorAll("button");
-let operand1;
-let operand2;
-let operator;
+let operand1 = "";
+let operand2 = "";
+let operator = null;
 
 // operation fuctions
 
@@ -34,6 +34,19 @@ function operate(operator, operand1, operand2) {
   }
 }
 
+// "operate()" using switch
+
+// switch (operator) {
+//   case "+":
+//     return add(operand1, operand2);
+//   case "-":
+//     return subtract(operand1, operand2);
+//   case "*":
+//     return multiply(operand1, operand2);
+//   case "/":
+//     return divide(operand1, operand2);
+// }
+
 // button event handling
 
 let calculation = [];
@@ -47,25 +60,43 @@ function calculate(button) {
     calculation = [];
     display.textContent = "0";
   } else if (value === "=") {
-    operand2 = append;
-    calculation = []
-    console.log(operand2)
-    operate(operator, operand1, operand2);
+    operand2 = parseFloat(append); // convert the string to a number
+    calculation = [];
+    let currentOperand = operate(operator, operand1, operand2);
+    display.textContent = currentOperand;
+    operand1 = currentOperand;
+    let currentOperator = []
+    operator = currentOperator;
   } else {
+    if (calculation.length === 0 && (value === "+" || value === "-" || value === "*" || value === "/")) {
+      // if the first button pressed after "=" is an operator, do nothing
+      return;
+    }
     calculation.push(value);
-    let append = calculation.join("")
-    display.textContent = append;
-    if (value === "+" || value === "-" || value === "x" || value === "/") {
-      operand1 = append;
-      operator = value.textContent;
-      calculation = [];
-      console.log(operand1)
-      console.log(operator)
+    if (value === "+" || value === "-" || value === "*" || value === "/") {
+      if (calculation.length === 1) {
+        // handle the case where an operator is entered as the first input
+        operator = value;
+        operand1 = 0;
+      } else {
+        operand1 = parseFloat(calculation.join(""));
+        operator = value;
+        calculation = [];
+      }
+    } else {
+      // update the display only when a non-operator button is pressed
+      append = calculation.join("");
+      display.textContent = append;
     }
   }
-
-  
 }
+
+
+// CURRENT ISSUES: 
+  // after doing the original equation, trying to do another equation with the previous solution causes the enter button the display
+  // trying to do consecutive equations only evaluates the last part of the equation (ex.: 10 * 2 + 3 will equal 5 because it is only calculating 2 + 3)
+
+
 
 // CURRENT OBJECTIVES:
 
